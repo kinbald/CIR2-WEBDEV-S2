39f836c4e8f776db01229d2fabab9ead33725fd8
billaud <billaudwilliam@gmail.com>
2017-05-08 14:23:05 +0200

    ajout mailer
    Ajout du mailer
    mot de passe de la boite mail à ajouter dans un fichier private_settings.php, situé dans le dossier App
    ce fichier doit contenir:
    <?php
    $password_mailer='le_mot_de_passe';

--- site/App/dependencies.php
+++ site/App/dependencies.php
@@ -11,19 +11,19 @@ $container['view'] = function (Psr\Container\ContainerInterface $c) {
     // Instantiate and add Slim specific extension
     $basePath = rtrim(str_ireplace('index.php', '', $c['request']->getUri()->getBasePath()), '/');
     $view->addExtension(new Slim\Views\TwigExtension($c['router'], $basePath));
-
     return $view;
 };
 
 //Override the default Not Found Handler
 //ERREUR 404 not found
 $container['notFoundHandler'] = function (Psr\Container\ContainerInterface $c) {
-    return function ($request, $response) use ($c) {
+    return function (\Slim\Http\Request $request,\Slim\Http\Response $response) use ($c) {
         return $c['view']->render($response->withStatus(404),'error/404.twig');
     };
 };
 
-$container['pdo']=function ($c){
+//mise en place de la connection avec la base de donnée
+$container['pdo']=function (Psr\Container\ContainerInterface $c){
     $settings = $c->get('settings')['database'];
     try{
         $db = new  PDO('pgsql:host=' . $settings['host'] . ';dbname=' . $settings['name'] . ';user=' . $settings['user'] . ';password=' . $settings['password']);
@@ -40,4 +40,15 @@ $container['pdo']=function ($c){
 
     }
     return $db;
+};
+
+//mise en place du mailer
+$container['mailer']=function (Psr\Container\ContainerInterface $c){
+    $settings=$c->get('settings')['smtp'];
+    $transport = Swift_SmtpTransport::newInstance($settings['host'], $settings['port'])
+        ->setUsername($settings['username'])
+        ->setPassword($settings['password'])
+        ->setEncryption('ssl')
+    ;
+    return  Swift_Mailer::newInstance($transport);
 };
