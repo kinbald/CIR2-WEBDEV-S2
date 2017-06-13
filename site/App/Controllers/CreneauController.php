e59814b2876829434ed2718068cdad207dc96078
billaud <billaudwilliam@gmail.com>
2017-06-13 14:51:13 +0200

    Ajout calendrier
    calendrier

--- site/App/Controllers/CreneauController.php
+++ site/App/Controllers/CreneauController.php
@@ -1,39 +1,51 @@
 <?php
-    /**
-     * Created by PhpStorm.
-     * User: kinbald
-     * Date: 13/06/17
-     * Time: 10:15
-     */
-    
-    namespace App\Controllers;
-    
-    
-    use App\Models\Creneau;
-    use Slim\Http\Request;
-    use Slim\Http\Response;
+/**
+ * Created by PhpStorm.
+ * User: kinbald
+ * Date: 13/06/17
+ * Time: 10:15
+ */
 
-    class CreneauController extends Controllers
+namespace App\Controllers;
+
+
+use App\Models\Creneau;
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Slim\Router;
+use Slim\Views\Twig;
+
+/**
+ * Class CreneauController
+ * @property Twig view
+ * @property Router router
+ * @package App\Controllers
+ */
+class CreneauController extends Controllers
+{
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @return Response
+     */
+    public function getMoisEnfant(Request $request, Response $response)
     {
-        /**
-         * @param Request $request
-         * @param Response $response
-         * @return
-         */
-        public function getMoisEnfant(Request $request, Response $response)
-        {
-            $params = $request->getParams();
-            $id_enfant = $request->getAttribute('id_enfant');
-            if( $this->checkInput($params, 'mois') && $this->checkInput($params, 'annee') )
-            {
-                $creneaux = (new Creneau())->getCreneauxMois($params['annee'], $params['mois'], $id_enfant);
-                return $response->withJson($creneaux);
-            }
-        }
-        
-        
-        public function checkInput($params, $name)
-        {
-            return isset($params[$name]) && !empty($params[$name]);
+        $params = $request->getParams();
+        $id_enfant = $request->getAttribute('id_enfant');
+        if ($this->checkInput($params, 'mois') && $this->checkInput($params, 'annee')) {
+            $creneaux = (new Creneau())->getCreneauxMois($params['annee'], $params['mois'], $id_enfant);
+            return $response->withJson($creneaux);
         }
     }
+
+
+    public function checkInput($params, $name)
+    {
+        return isset($params[$name]) && !empty($params[$name]);
+    }
+
+    public function calendrier(Request $request, Response $response, $args)
+    {
+        return $this->view->render($response,'calendrier.twig');
+    }
+}
--- site/App/routes.php
+++ site/App/routes.php
@@ -1,18 +1,18 @@
 <?php
 // Routes
 use App\Controllers\TestController;
-$app->group('',function () use ($app)
-{
-$app->get('/b',\App\Controllers\TestController::class)->setName('test');
 
-$app->get('/salut[/{nom}]',\App\Controllers\TestController::class.':salut')->setName("salut");
+$app->group('', function () use ($app) {
+    $app->get('/b', \App\Controllers\TestController::class)->setName('test');
+
+    $app->get('/salut[/{nom}]', \App\Controllers\TestController::class . ':salut')->setName("salut");
 
 // setName permet d'appeler path_for('nom_route',{param}) dans twig
-$app->get('/index', \App\Controllers\UserController::class.':getIndex')->setName("index");
+    $app->get('/index', \App\Controllers\UserController::class . ':getIndex')->setName("index");
 
-$app->get('/excel[/]',TestController::class.':excel');
+    $app->get('/excel[/]', TestController::class . ':excel');
 
-$app->get('/mail[/]',TestController::class.':mail');
+    $app->get('/mail[/]', TestController::class . ':mail');
 
 
 })->add(new \App\Middleware\Authentification($container));
@@ -20,21 +20,21 @@ $app->get('/mail[/]',TestController::class.':mail');
 /**
  * Routes de base pour la page de connexion
  */
-$app->group('', function () use ($app)
-{
+$app->group('', function () use ($app) {
     $app->get('/login', 'AuthController:getLogin')->setName('login.get');
     $app->post('/login', 'AuthController:postLogin')->setName('login.post');
 })->add(new \App\Middleware\ValidationErreursMiddleware($container))
     ->add(new \App\Middleware\PersitenceFormulaireMiddleware($container));
-    
-$app->group('', function () use ($app)
-{
-    $app->post('/ajax/calendrier/{id_enfant}', \App\Controllers\CreneauController::class.':getMoisEnfant')->setName('AJAX-getMoisEnfant');
+
+$app->group('', function () use ($app) {
+    $app->post('/ajax/calendrier/{id_enfant}', \App\Controllers\CreneauController::class . ':getMoisEnfant')->setName('AJAX-getMoisEnfant');
 });
 
 
-$app->get('/logout',App\Controllers\AuthController::class.':logout')->setName("logout");
-$app->get('/recover',App\Controllers\AuthController::class.':recover')->setName("recover.get");
-$app->post('/recover',App\Controllers\AuthController::class.':sendRecover')->setName("recover.post");
-$app->get('/recover/{token}',App\Controllers\AuthController::class.':token')->setName("recoverToken.get");
-$app->post('/recover/{token}',App\Controllers\AuthController::class.':tokenValidation')->setName("recoverToken.post");
+$app->get('/logout', App\Controllers\AuthController::class . ':logout')->setName("logout");
+$app->get('/recover', App\Controllers\AuthController::class . ':recover')->setName("recover.get");
+$app->post('/recover', App\Controllers\AuthController::class . ':sendRecover')->setName("recover.post");
+$app->get('/recover/{token}', App\Controllers\AuthController::class . ':token')->setName("recoverToken.get");
+$app->post('/recover/{token}', App\Controllers\AuthController::class . ':tokenValidation')->setName("recoverToken.post");
+
+$app->get('/calendrier/{id}',App\Controllers\CreneauController::class.':calendrier')->setName("calendrier");
