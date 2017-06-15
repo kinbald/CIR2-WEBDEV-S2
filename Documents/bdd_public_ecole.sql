088fcb2167d0c03566ec8ba1423b1d1ad83a67f9
billaud <billaudwilliam@gmail.com>
2017-06-15 18:28:04 +0200

    sql

---
+++ Documents/bdd_public_ecole.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.ecole (id_ecole, nom_ecole) VALUES (1, 'Marie Mauron');
+INSERT INTO public.ecole (id_ecole, nom_ecole) VALUES (2, 'Jean Giono');
+INSERT INTO public.ecole (id_ecole, nom_ecole) VALUES (3, 'P Pagnol');
+INSERT INTO public.ecole (id_ecole, nom_ecole) VALUES (4, 'Jean Aicard');
+INSERT INTO public.ecole (id_ecole, nom_ecole) VALUES (5, 'Jules Audibert');
+INSERT INTO public.ecole (id_ecole, nom_ecole) VALUES (6, 'Jules Ferry');
+INSERT INTO public.ecole (id_ecole, nom_ecole) VALUES (7, 'M Pagnol');
+INSERT INTO public.ecole (id_ecole, nom_ecole) VALUES (8, 'Louis Palazy');
---
+++ Documents/bdd_public_enfant.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.enfant (id_enfant, nom_enfant, prenom_enfant, date_naissance_enfant) VALUES (1, 'billaud', 'lucas', '2008-03-13');
+INSERT INTO public.enfant (id_enfant, nom_enfant, prenom_enfant, date_naissance_enfant) VALUES (2, 'billaud', 'tristan', '2005-08-12');
+INSERT INTO public.enfant (id_enfant, nom_enfant, prenom_enfant, date_naissance_enfant) VALUES (3, 'billaud', 'robin', '2001-10-19');
+INSERT INTO public.enfant (id_enfant, nom_enfant, prenom_enfant, date_naissance_enfant) VALUES (4, 'artru', 'manon', '2001-01-14');
