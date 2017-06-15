0c07741238924d6e1ed185ae7590326530e27fa3
Thoril <artru.thomas.monchal@gmail.com>
2017-06-15 22:21:08 +0200

    Fix affichage des données

--- site/public/js/exportData.js
+++ site/public/js/exportData.js
@@ -1,8 +1,7 @@
 /**
  * Created by thomas on 14/06/17.
  */
-function ajaxExportDataGetClasses()
-{
+function ajaxExportDataGetClasses() {
     var nom_ecole = $("#ecole option:selected").text();
     var data = {nom_ecole: nom_ecole};
     $.ajax({
@@ -15,16 +14,15 @@ function ajaxExportDataGetClasses()
         $("#classe").empty();
         $("#classe").append('<option value="0" >Sélectionner Classe</option>');
         $.each(response, function (key, value) {
-            console.log(key,  value);
+            console.log(key, value);
             $("#classe").append('<option value="' + key + '" >' + value + '</option>');
         });
     });
 }
-function ajaxExportDataGetPlanning()
-{
+function ajaxExportDataGetPlanning() {
     var nom_classe = $("#classe option:selected").text();
     var nom_ecole = $("#ecole option:selected").text();
-    var data = {nom_classe: nom_classe,nom_ecole: nom_ecole};
+    var data = {nom_classe: nom_classe, nom_ecole: nom_ecole};
     $.ajax({
         type: 'POST',
         url: 'ajax/exportDataGetPlanning',
@@ -35,10 +33,11 @@ function ajaxExportDataGetPlanning()
         $("#resultat").empty();
         $("#resultat").append('<table>');
         $("#resultat").append('<tr><td>NOM</td><td>PRENOM</td><td>ACTIVITÉ</td></tr>');
-            for(enfant in response){
-             $("#resultat").append('<tr><td>'+enfant.nom_enfant+'</td><td>'+enfant.prenom_enfant+'</td><td>'+enfant.intitule+'</td></tr>');
-            }
+        for(var key in response){
+            var enfant=response[key];
+            $("#resultat").append('<tr><td>'+enfant.nom_enfant+'</td><td>'+enfant.prenom_enfant+'</td><td>'+enfant.intitule+'</td></tr>');
+        }
         $("#resultat").append('</table>');
-        $("#resultat").append('<a href="'+'">Télécharger le Planning</a>');
+        $("#resultat").append('<a href="' + '">Télécharger le Planning</a>');
     });
 }
