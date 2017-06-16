303fb9552f4d3fc86dbf51fd7a6581703114fe18
billaud <billaudwilliam@gmail.com>
2017-06-16 11:59:18 +0200

    Activite
    page ajout activite

--- site/App/Controllers/ActiviteController.php
+++ site/App/Controllers/ActiviteController.php
@@ -9,43 +9,41 @@
 namespace App\Controllers;
 
 use App\Models\Activite;
-use App\Models\Admin;
-use App\Models\Ecole;
-use App\Models\Enfant;
-use App\Models\Est_responsable_de;
-use App\Models\Responsable_legal;
 use Slim\Http\Request;
 use Slim\Http\Response;
-use Slim\Views\Twig;
 
 
 class ActiviteController extends Controllers
 {
-    public function getActivite(Request $request, Response $response){
+    public function getActivite(Request $request, Response $response)
+    {
         return $this->view->render($response, 'activite-admin.twig');
     }
 
-    public function postActivite(Request $request, Response $response){
+    public function postActivite(Request $request, Response $response)
+    {
         // Tableau qui contiendra les erreurs
-        $errors = array(null);
+        $errors = array();
         // Récupération des paramètres
         $post = $request->getParams();
-        if(isset($post['intitule']) && isset($post['classname'])){
-            if(empty($post['intitule']) || empty($post['classname'] || empty($post['date_naissance_enfant']))) {
-                $errors['erreur'] = "Tous les champs sont obligatoires";
-            } else{
-
-                $activite = new Activite();
-                $activite->insertActivite($post['intitule'], $post['classname']);
-                return $response->withRedirect($this->router->pathFor('index-admin'));
+        if (empty($post['intitule']) || empty($post['classname'] || empty($post['date_naissance_enfant']))) {
+            $errors['erreur'] = "Tous les champs sont obligatoires";
+        } else {
+            $p=array('intitule'=>$post['intitule'],'classname'=>$post['classname']);
+            if(empty((new Activite())->select($p))){
+                (new Activite())->insert($p);
+            }else{
+                $errors['duplication']="une classe identique existe déjà";
             }
 
+            return $this->view->render($response, 'activite-admin.twig');
         }
+
 
         // Il y a des erreurs donc on les garde dans la session pour l'affichage
         $_SESSION['errors'] = $errors;
         // Redirection vers le formulaire
-        return $response->withRedirect($this->router->pathFor('activite-admin.get'));
+        return $this->view->render($response, 'activite-admin.twig');
 
     }
 
--- site/App/routes.php
+++ site/App/routes.php
@@ -65,17 +65,19 @@ $app->group('/admin/', function () use ($app,$container) {
     $app->get('importData', \App\Controllers\ImportDataController::class . ':getImportData')->setName("importData.get");
     $app->post('importData', \App\Controllers\ImportDataController::class . ':postImportData')->setName("importData.post");
 
-    $app->group('classe', function () use ($app) {
-        $app->get('', \App\Controllers\ClasseController::class . ':getClasse')->setName("classe-admin.get");
-        $app->post('', \App\Controllers\ClasseController::class . ':postClasse')->setName("classe-admin.post");
+    $app->group('form/', function () use ($app) {
+        $app->get('classe', \App\Controllers\ClasseController::class . ':getClasse')->setName("classe-admin.get");
+        $app->post('classe', \App\Controllers\ClasseController::class . ':postClasse')->setName("classe-admin.post");
+
+        $app->get('activite', \App\Controllers\ActiviteController::class . ':getActivite')->setName("activite-admin.get");
+        $app->post('activite', \App\Controllers\ActiviteController::class . ':postActivite')->setName("activite-admin.post");
     })->add(new \App\Middleware\ValidationErreursMiddleware($container))
         ->add(new \App\Middleware\PersitenceFormulaireMiddleware($container));
 
     $app->get('enfant', \App\Controllers\EnfantController::class . ':getEnfant')->setName("enfant-admin.get");
     $app->post('enfant', \App\Controllers\EnfantController::class . ':postEnfant')->setName("enfant-admin.post");
 
-    $app->get('activite', \App\Controllers\ActiviteController::class . ':getActivite')->setName("activite-admin.get");
-    $app->post('activite', \App\Controllers\ActiviteController::class . ':postActivite')->setName("activite-admin.post");
+
 
 
     $app->post('index', App\Controllers\AuthAdminController::class . ':insertRespoLegal')->setName('add-user.post');
